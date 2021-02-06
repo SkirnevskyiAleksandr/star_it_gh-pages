5d25721aff91924344c3ff4b13439e5427a1f732
millisabel <38396309+millisabel@users.noreply.github.com>
2021-02-06 18:16:20 +0200

    fix: refactoring Hero component: title, nesting, background

--- landing/business-landing/src/components/Hero/Hero.js
+++ landing/business-landing/src/components/Hero/Hero.js
@@ -3,30 +3,26 @@ import Download from "../Download/Download";
 
 import "./style/hero.css";
 import "./style/description.css";
-import "./style/visual.css";
+import "./style/circle.css";
 
 import img from "./img/foto.png";
 
 const Hero = () => {
     return (
-        <div className="hero">
-            <div className="row">
-                <div className="hero__wrap">
-                    <div className="hero__item">
-                        <img src={img} alt="img" className="hero__img" width="400" height="595"/>
-                    </div>
-                    <div className="hero__item">
-                        <div className="description">
-                            <h2 className="description__title"><span className="description__mark">Маркетинг</span> без обратной связи - как современный телевизор без пульта</h2>
-
-                            <p className="description__text">StarIt! позволит вам узнавать недостатки и преимущества вашего продукта без лишних телодвижений</p>
-                            <Download/>
-                        </div>
-                    </div>
-                </div>
-                <div className="visual"/>
+        <section className="hero row">
+            <div className="hero__item">
+                <img src={img} alt="img" className="hero__img" width="400" height="595"/>
             </div>
-        </div>
+            <div className="hero__item circle">
+                <div className="description">
+                    <h1 className="description__text">StarIt! позволит вам узнавать недостатки и преимущества вашего
+                        продукта без лишних телодвижений</h1>
+                    <p className="description__title"><span className="description__mark">Маркетинг</span> без обратной
+                        связи - как современный телевизор без пульта</p>
+                    <Download/>
+                </div>
+            </div>
+        </section>
     );
 };
 
