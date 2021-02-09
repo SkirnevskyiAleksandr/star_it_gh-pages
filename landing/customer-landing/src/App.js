ea967d816f6656304519c0b0fe6b122997d558cc
Aleksandr <sanya-sha2016@ukr.net>
2021-02-09 16:28:39 +0200

    feat: add block Dowload

--- landing/customer-landing/src/App.js
+++ landing/customer-landing/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Download from './components/Download/download';
 
 
 function App() {
   return (
     <div className = " page">
       <section className = "wrapper">
-        
+        <Download/>
       </section>  
     </div>
     
