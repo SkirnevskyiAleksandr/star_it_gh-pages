85c79510fa0f56a861a94b002d5970ab1c22c288
Aleksandr <sanya-sha2016@ukr.net>
2021-02-10 21:28:40 +0200

    feat: add responsive to HowItWorcksItem

--- landing/customer-landing/src/components/Footer/footer.js
+++ landing/customer-landing/src/components/Footer/footer.js
@@ -7,7 +7,7 @@ const Footer = () =>{
       <section className = {s.footerCcontacts}>
         <div className={s.footerCcontacts__title}>Наши контакты</div>
         <address className = {s.footerContacts__address}>
-          <a href="tel+377777777" className = {s.footerCcontacts__item}>+3777777777</a>
+          <a href="tel+377777777" className = {s.footerContacts__item}>+3777777777</a>
           <a href="mailto:sanya" className = {s.footerContacts__item}>тыц</a>
 
         </address>
