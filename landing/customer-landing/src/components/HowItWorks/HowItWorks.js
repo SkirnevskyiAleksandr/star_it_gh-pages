c68f66a7029ee3922ddd5586f455ac3dc6ceb226
Aleksandr <sanya-sha2016@ukr.net>
2021-02-10 18:57:19 +0200

    feat:make HowItWorksItem individual component

--- landing/customer-landing/src/components/HowItWorks/HowItWorks.js
+++ landing/customer-landing/src/components/HowItWorks/HowItWorks.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import "./HowItworks.css";
-
+import HowItWorksItem1 from './HowItWorksItems/HowItWorkstem1'
+import HowItWorksItem2 from './HowItWorksItems/HowItWorkstem2'
+import HowItWorksItem3 from './HowItWorksItems/HowItWorkstem3'
 const HowItWorks = () => {
     return (
     <div>
         <div class="how-it-works__header">Как это работает</div>
       <div class="how-it-works">
-        <div class="how-it-works__item">Сканируйте  QR-код</div>
-        <div class="how-it-works__item">Оцениваете сервис</div>
-        <div class="how-it-works__item">Сеть делает свой сервис лучше</div>
+        <HowItWorksItem1/>
+        <HowItWorksItem2/>
+        <HowItWorksItem3/>
       </div>
     </div>  
     );
