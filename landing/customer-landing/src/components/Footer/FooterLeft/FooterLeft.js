a3fd75f7d2772ef507bda59170cf943f910919ac
Aleksandr <sanya-sha2016@ukr.net>
2021-02-12 19:07:57 +0200

    feat: add footer

--- landing/customer-landing/src/components/Footer/FooterLeft/FooterLeft.js
+++ landing/customer-landing/src/components/Footer/FooterLeft/FooterLeft.js
@@ -1,13 +1,25 @@
 import React from 'react';
 import s from './footerLeft.module.css';
+import Facebook from './SocialIcons/Facebook/facebook';
+import Twitter from './SocialIcons/twitter/twitter';
+import Instagram from './SocialIcons/instagram/instagram';
 
 const FooterLeft = () =>{
     return(
 <section className = {s.footerContacts}>
-        <div className={s.footerContacts__title}>Наши контакты</div>
+        <b className = {s.footerContacts__title}>YourR&D</b>
         <address className = {s.footerContacts__address}>
-          <a href="tel+377777777" className = {s.footerContacts__item}>+3777777777</a>
-          <a href="mailto:sanya" className = {s.footerContacts__item}>тыц</a>
+          <div>
+                    <a href="tel+377777777" className = {s.footerContacts__item}>+3777777777</a>
+          </div>
+          <div>
+            <a href="mailto:sanya" className = {s.footerContacts__item}>E-mail</a>
+          </div>
+          <div className = {s.socialIcons}>
+            <Facebook/>
+            <Twitter/>
+            <Instagram/>
+          </div>
         </address>
       </section>
     )
