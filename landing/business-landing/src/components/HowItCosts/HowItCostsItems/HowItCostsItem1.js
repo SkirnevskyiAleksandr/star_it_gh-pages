5b710ab71bebc6730f4c02928cba6385467480af
Aleksandr <sanya-sha2016@ukr.net>
2021-02-12 22:56:35 +0200

    refactor:premium card

--- landing/business-landing/src/components/HowItCosts/HowItCostsItems/HowItCostsItem1.js
+++ landing/business-landing/src/components/HowItCosts/HowItCostsItems/HowItCostsItem1.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import s from './HowItCostsItem.module.css';
+import picture from './icon.png';
 
 const HowItCostsItem1 = () =>{
     return(
-        <div className={s.HowItCosts__item}>Сканируйте  QR-код</div> 
+        <section className={s.HowItCosts__item}>
+            <div className = {s.card__top}>
+                <div className = {s.iconWrapper}>
+                    jljlkj
+                </div>
+               <div> Star-It Premium</div> 
+            </div>
+            <div></div>
+        </section> 
     )
 };
 
