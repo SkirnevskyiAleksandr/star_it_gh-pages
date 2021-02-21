0c80dcea43d704711e288b4ef267e2d214ab6d0e
VadimSido <vadsid74@gmail.com>
2021-02-21 15:30:46 +0200

    fix: added object to component WhereItWork and style

--- landing/customer-landing/src/components/WhereItWork/WhereItWork.js
+++ landing/customer-landing/src/components/WhereItWork/WhereItWork.js
@@ -1,27 +1,32 @@
 import React from 'react';
 import styles from './WhereItWork.module.css';
 import WhereItWorkItem from './WhereItWorkItem';
+import Ok_logo from './images/shopOk.png';
+import Wait_logo from './images/wait.png';
+import Atm_logo from './images/ATM.png';
+import People_logo from './images/people.png';
+import Buy_logo from './images/buy.png';
 
 const content = [
     {
         title: 'Обслуживание на кассе оказалось выше всяких похвал, Вам очень понравилось',
-        logo:
+        logo: Ok_logo
     },
     {
         title: 'Вы долго ждете продавца-консультанта, а его все нет и нет',
-        logo:
+        logo: Wait_logo
     },
     {
         title: 'Банкомат грязный, подступ неудобный, кнопки залипают',
-        logo:
+        logo: Atm_logo
     },
     {
         title: 'Вы хотите порекомендовать другим покупателям или предостеречь их от покупки товара',
-        logo:
+        logo: People_logo
     },
     {
         title: 'Вы хотите узнать, стоит ли брать тот или иной товар',
-        logo:
+        logo: Buy_logo
     }
 ]
 
