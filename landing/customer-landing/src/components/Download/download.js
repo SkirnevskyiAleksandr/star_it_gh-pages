ea74b3da2da513b9a49a4fb7a6481dc5522cb2c7
millisabel <38396309+millisabel@users.noreply.github.com>
2021-02-22 18:20:39 +0200

    feat: added components Download and DownloadsItem

--- landing/customer-landing/src/components/Download/download.js
+++ landing/customer-landing/src/components/Download/download.js
@@ -1,12 +1,40 @@
 import React from 'react';
-import s from './download.module.css';
+import DownloadItem from "./DownloadItem";
+
+import download from "./Download.module.css";
+
+import google from "./img/google.svg";
+import ios from "./img/ios.svg";
+
 
 const Download = () => {
+
+    const downloadLinks = [
+        {
+            link: "#",
+            title: "скачать с Google Play",
+            imgUrl: google,
+        },
+        {
+            link: "#",
+            title: "скачать с App Story",
+            imgUrl: ios,
+        },
+    ];
+
+    const items = downloadLinks.map((item, i) =>
+        <DownloadItem
+            key = {i}
+            link = {item.link}
+            title = {item.title}
+            imgUrl = {item.imgUrl}
+        />);
+
     return (
-        <div className = {s.download}>
-          <h4 className = {s.buttonDownload__title2}> Начни менять жизнь к лучшему</h4>
-          <button className = {s.download__button} >Скачай приложение</button>
+        <div className={download.container}>
+            {items}
         </div>
     );
 };
+
 export default Download;
