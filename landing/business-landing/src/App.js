0ea1988c3e44d1490702cea6d5d34c1459b2924a
millisabel <38396309+millisabel@users.noreply.github.com>
2021-03-04 17:01:43 +0200

    fixed: refactoring

--- landing/business-landing/src/App.js
+++ landing/business-landing/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Header from "./components/Header/Header";
-import Hero from "./components/Hero/Hero";
+import Header from "./components/Header/";
+import Hero from "./components/Hero/";
 
 function App() {
   return (
