8882059b06ce260b14c31155e42a3c3a338d42a2
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-04-13 23:15:34 +0300

    init:customer-mobile-window

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,35 +1,78 @@
 import React from 'react';
 import './App.css';
-import logo from './asets/logo_silpo.png'
+import logo from './asets/logo_silpo.png';
+import Clock from './asets/Clock/clock';
+import Rating from './asets/Rating/rating';
+import  './asets/Rating/rating.css';
 
 function App() {
   return (
     <div className="App">
-    <section className = "data">
-      <div className = "data_wrapper">
-    <div className = ""> 
-      <div className = "data__item">
-          <span className = "data__item_name">Name: </span>
-          <span className = "item_info">Silpo Ltd</span>
-      </div>
+      {/* header */}
+      <section className="header">
 
-      <div className = "data__item address">
-        <span className = "data__item_address">Address: </span>
-        <span className = "item_info">Kiev, Str. Bulgakov, 11</span>
-      </div>
-    </div>
-    <div className = "logo__wrapper"><img src = {logo} className = "logo" alt="img"/></div>
-    </div>
-    
-      <div className = "data__item ticket">tiket</div>
-      <div className = "data__item time">time</div>
-    </section>
-    <section className = "rating">
-    </section> 
-    <section className = "feedbeak">
-    </section>
+      </section>
+      <nav className="nav">
+        <a href="#" className="nav__items">statistics</a>
+        <a href="#" className="nav__items">info</a>
+        <a href="#" className="nav__items">profile</a>
+      </nav>
+      <section className="data">
+        <div className="data_wrapper">
+          <div className="">
+            <div className="data__item">
+              <span className="data__item_name">Name: </span>
+              <span className="item_info">Silpo Ltd</span>
+            </div>
 
-    </div>  
+            <div className="data__item address">
+              <span className="data__item_address">Address: </span>
+              <span className="item_info">Kiev, Str. Bulgakov, 11</span>
+            </div>
+            <div className="data__item ticket">
+              <span>Ticket window:</span>
+              <span className="item_info"> №8</span>
+            </div>
+          </div>
+          <div className="logo__wrapper">
+            <img src={logo} className="logo" alt="img" />
+          </div>
+        </div>
+
+        <div className="time-wrapper">
+        <div className="data__item time">
+          <div className="icon-clock">
+            <span className="clock-letter">L</span>
+          </div>
+          <span className="time-text"> Time</span>
+        </div>
+        
+          <Clock />
+        
+        </div>
+        <hr className="line"/>
+
+      </section>
+      <section className="rating-section">
+        <Rating/>
+        <hr className="line"/>
+      </section>
+      <section className="feedback">
+        <form className="feedback__form">
+          <div className="checkbox__wrapper">
+          <label className="checkbox__label">
+            <span className="checkbox__text"> I want to get answer</span>
+          <input type="checkbox" className="checkbox"/>
+          </label>
+          </div>
+          <input className="input__feedback" type="textarea" placeholder="feedback"/>
+          <button className="button" type="submit">
+            Send
+          </button>
+        </form>
+      </section>
+
+    </div>
   );
 }
 
---
+++ customer/src/asets/Clock/clock.js
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import './clock.css'
+
+class Clock extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: new Date().toLocaleString(),
+    };
+  }
+  componentDidMount() {
+    this.intervalID = setInterval(() => this.tick(), 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.intervalID);
+  }
+  tick() {
+    this.setState({
+      time: new Date().toLocaleString(),
+    });
+  }
+  render() {
+    return <span className="App-clock"> {this.state.time}.</span>;
+  }
+}
+
+export default Clock;
