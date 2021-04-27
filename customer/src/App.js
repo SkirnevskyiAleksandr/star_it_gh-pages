147c26e97a3f8fc7a26c147577de0ebbcf534800
SkirnevskyiAleksandr <sanya-sha2016@ukr.net>
2021-04-28 00:07:29 +0300

    feat: GET

--- customer/src/App.js
+++ customer/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import './App.css';
 import logo from './asets/logo_silpo.png';
-import ATB from './asets/logo_atb.svg';
 import Clock from './asets/Clock/clock';
-import icon from './asets/clock-icon.svg';
-
+import icon from './asets/clock-icon.svg';	
+const zapros = function zapros() { 
+	fetch("./server.json").then( response => response.json()).then(json=>console.log(json));
+};
 function App() {
+
   return (
     <div className="App">
       {/* header */}
       <section className="header">
      
       </section>
+	
       <nav className="nav">
-        <a href="#" className="nav__items">kishenya</a>
+        <a onClick={zapros}  href="#" className="nav__items">kishenya</a>
         <a href="#" className="nav__items">atb</a>
         <a href="#" className="nav__items">silpo</a>
       </nav>
@@ -61,15 +64,15 @@ function App() {
             <div className="output">Rating</div>
             <div className="rating">
                 <input type="radio" id="star_5" name="star" value="5"/>
-                <label for="star_5"></label>
+                <label htmlFor="star_5"></label>
                 <input type="radio" id="star_4" name="star" value="4"/>
-                <label for="star_4"></label>
+                <label htmlFor="star_4"></label>
                 <input type="radio" id="star_3" name="star" value="3"/>
-                <label for="star_3"></label>
+                <label htmlFor="star_3"></label>
                 <input type="radio" id="star_2" name="star" value="2"/>
-                <label for="star_2"></label>
+                <label htmlFor="star_2"></label>
                 <input type="radio" id="star_1" name="star" value="1"/>
-                <label for="star_1"></label>
+                <label htmlFor="star_1"></label>
 		    </div>
         <hr className="line"/>
         </div>
@@ -81,8 +84,14 @@ function App() {
             <span className="checkbox__text"> I want to get answer</span>
           <input type="checkbox" className="checkbox" name="checkbox"/>
           </label>
-          </div>
-          <input className="input__feedback" type="textarea" name="feedback" placeholder="feedback"/>
+		 </div>
+          <input className="input__feedback"
+            required
+            type="textarea"
+            name="feedback"
+            minLength="2"
+            maxLength="25"
+            placeholder="feedback"/>
           <button className="button" type="submit">
             Send
           </button>
